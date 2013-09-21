b7cb26cf255cb03d718f53de13b524e654b7c59d
Frau Sma <jana@plokta.org>
2013-09-21 19:16:53 +0200

    Replace auto-generated test comments with TODO.

--- bookloupe.rb
+++ bookloupe.rb
@@ -16,11 +16,6 @@ class Bookloupe < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test bookloupe`.
-    system "false"
+    # TODO: integrate Bookloupe test suite
   end
 end
